68cf3c2d2d623301e22bc54257031a0da0a2445e
hyosin-Jang <71035113+hyosin-Jang@users.noreply.github.com>
2021-02-23 11:56:50 +0900

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -45,7 +45,7 @@ const IndexPage = () => {
                         <div className={"col-8"}>
                             <h1>클릭 한 번으로<br/>나에게 맞는<br/>우리동네 취미강좌 추천</h1>
                             <div className={"my-button"} onClick={executeScroll}>
-                                앱 출시 알림 받기
+                                앱 다운로드 받기
                             </div>
                             <p>이미 <b>370명</b>이 출시 알림을 신청했습니다.</p>
                         </div>
